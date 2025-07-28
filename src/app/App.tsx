0235a0c36869626a90a3c56820e4e1a9faf2425e
Michael1221994 <103842937+Michael1221994@users.noreply.github.com>
2025-07-28 21:44:53 +0300

    Organizations page in contribution app for admin view is done.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import DeclarationForm from '../Pages/Contribution_Declaration/ContributionDecla
 import Header_Section from '../Components/Invoice/Header_Section';
 import Invoice from '../Components/Invoice/Invoice';
 import Declaration_History from '../Components/Client Side Declaration Management/Declaration_History';
+import Organizations from '../Components/Organization List Backoffice/Organizations';
 function App() {
   // const DeclarationForm = React.lazy(() => import('../Pages/DeclarationForm'));
 
@@ -13,7 +14,8 @@ function App() {
       {/* <DeclarationForm /> */}
       {/* <Header_Section /> */}
       {/* <Invoice /> */}
-      <Declaration_History/>
+      {/* <Declaration_History/> */}
+      <Organizations/>
     
     </>
   )
